169f29f74529c045513c310fd7b0c9a87eeec6e1
devoloper mickey <arpanbera475@gmail.com>
2025-01-21 09:45:19 +0530

    some changes!

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "https://portfolio2-lemon-chi.vercel.app/", // Adjust as needed (e.g., "http://your-frontend-domain.com")
+  origin: "https://portfolio2-lemon-chi.vercel.app/",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
